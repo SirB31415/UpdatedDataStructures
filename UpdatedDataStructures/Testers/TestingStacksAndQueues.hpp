1d71fed33d3e364775ed4075f63ebc57120e82ea
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2018-05-18 13:05:41 -0600

    Added the testing stacks and queues class.

---
+++ UpdatedDataStructures/Testers/TestingStacksAndQueues.hpp
@@ -0,0 +1,29 @@
+//
+//  TestingStacksAndQueues.hpp
+//  UpdatedDataStructures
+//
+//  Created by Brailow, Parker on 5/18/18.
+//  Copyright © 2018 Brailow Inc. All rights reserved.
+//
+
+#ifndef TestingStacksAndQueues_hpp
+#define TestingStacksAndQueues_hpp
+
+#include <iostream>
+#include </Users/pbra1660/Documents/Cplusplus/UpdatedDataStructures/UpdatedDataStructures/Controller/Tools/Timer.hpp>
+#include </Users/pbra1660/Documents/Cplusplus/UpdatedDataStructures/UpdatedDataStructures/Controller/FileController.hpp>
+#include </Users/pbra1660/Documents/Cplusplus/UpdatedDataStructures/UpdatedDataStructures/Model/Structures/Queue.hpp>
+#include </Users/pbra1660/Documents/Cplusplus/UpdatedDataStructures/UpdatedDataStructures/Model/Structures/Stack.hpp>
+#include </Users/pbra1660/Documents/Cplusplus/UpdatedDataStructures/UpdatedDataStructures/Model/Structures/List.hpp>
+using namespace std;
+
+class TestingStackAndQueues
+{
+public:
+    void stackVersusList();
+    void stackVersusQueue();
+    void queueVersusList();
+};
+
+
+#endif /* TestingStacksAndQueues_hpp */
